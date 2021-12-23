e8520baf3b1a05e4ece41144afd337d09332bd34
vietth <tranhoangviet@gapo.com.vn>
2021-12-23 17:45:49 +0700

    update setting

--- lib/features/setting/security/setting_security.dart
+++ lib/features/setting/security/setting_security.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:auto_route/src/router/auto_router_x.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
@@ -41,13 +40,28 @@ class SettingSecurityScreen extends StatelessWidget {
                             style: ROITitleLargeTextStyle(
                                 color: provider.themeMode.text),
                           ),
-                          SizedBox(
+                          const SizedBox(
                             height: 260,
                             child: TabBarView(
                               children: [
-                                _WalletAddressTab(),
-                                _WalletAddressTab(),
-                                _WalletAddressTab(),
+                                _WalletAddressTab(
+                                  qrCode:
+                                      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png',
+                                  address:
+                                      'X-fuji1qen0kzvn34zc8jsdatym8tacukgdfepyk9ees2',
+                                ),
+                                _WalletAddressTab(
+                                  qrCode:
+                                      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png',
+                                  address:
+                                      'X-fuji1qen0kzvn34zc8jsdatym8tacukgdfepyk9ees2',
+                                ),
+                                _WalletAddressTab(
+                                  qrCode:
+                                      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png',
+                                  address:
+                                      'X-fuji1qen0kzvn34zc8jsdatym8tacukgdfepyk9ees2',
+                                ),
                               ],
                             ),
                           ),
@@ -68,15 +82,50 @@ class SettingSecurityScreen extends StatelessWidget {
                                   color: provider.themeMode.primary),
                               labelColor: provider.themeMode.primary,
                               unselectedLabelStyle: ROITitleLargeTextStyle(
-                                  color: provider.themeMode.secondary60),
+                                  color: provider.themeMode.text40),
                               unselectedLabelColor:
-                                  provider.themeMode.secondary60,
+                                  provider.themeMode.text40,
                               tabs: [
                                 Tab(text: Strings.current.sharedXChain),
                                 Tab(text: Strings.current.sharedCChain),
                                 Tab(text: Strings.current.sharedPChain),
                               ],
                             ),
+                          ),
+                          const SizedBox(height: 24),
+                          Divider(color: provider.themeMode.text10),
+                          const SizedBox(height: 16),
+                          Text(
+                            Strings.current.sharedPrivateKey,
+                            style: ROITitleLargeTextStyle(
+                                color: provider.themeMode.text),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            Strings.current.settingSecurityPrivateKeyNote,
+                            style: ROIBodySmallTextStyle(
+                                color: provider.themeMode.text70),
+                          ),
+                          const SizedBox(height: 16),
+                          _WalletKeyInfoWidget(
+                            title: Strings.current.sharedPrivateKey,
+                            content:
+                                'PrivatekeyHbot5UuF4d65zDPtDGPNFLZacAJku7ErTwK2otD4iwejoywne',
+                          ),
+                          const SizedBox(height: 16),
+                          _WalletKeyInfoWidget(
+                            title: Strings.current.settingSecurityCPrivateKey,
+                            content:
+                                'PrivatekeyHbot5UuF4d65zDPtDGPNFLZacAJku7ErTwK2otD4iwejoywne',
+                          ),
+                          const SizedBox(height: 24),
+                          Divider(color: provider.themeMode.text10),
+                          const SizedBox(height: 16),
+                          _WalletKeyInfoWidget(
+                            title: Strings.current.sharedPassphrase,
+                            titleColor: provider.themeMode.text,
+                            content:
+                                'blouse airport promote orchard glare phone scrap make siren battle settle trade capable urban lecture vacuum prepare oil keep decide saddle inform thunder option',
                           ),
                         ],
                       ),
@@ -93,6 +142,13 @@ class SettingSecurityScreen extends StatelessWidget {
 }
 
 class _WalletAddressTab extends StatelessWidget {
+  final String qrCode;
+  final String address;
+
+  const _WalletAddressTab(
+      {Key? key, required this.qrCode, required this.address})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Consumer<WalletThemeProvider>(
@@ -101,14 +157,13 @@ class _WalletAddressTab extends StatelessWidget {
         child: Column(
           children: [
             CachedNetworkImage(
-              imageUrl:
-                  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png",
-              width: double.infinity,
+              imageUrl: qrCode,
+              width: 200,
               height: 200,
               imageBuilder: (context, imageProvider) => Container(
                 decoration: BoxDecoration(
                   image: DecorationImage(
-                      image: imageProvider, fit: BoxFit.fitHeight),
+                      image: imageProvider, fit: BoxFit.fitWidth),
                 ),
               ),
               placeholder: (context, url) =>
@@ -121,12 +176,50 @@ class _WalletAddressTab extends StatelessWidget {
               fadeOutDuration: const Duration(milliseconds: 0),
             ),
             Text(
-              'X-fuji1qen0kzvn34zc8jsdatym8tacukgdfepyk9ees2',
+              address,
               textAlign: TextAlign.center,
               style: ROIBodyLargeTextStyle(color: provider.themeMode.text),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _WalletKeyInfoWidget extends StatelessWidget {
+  final String title;
+  final Color? titleColor;
+  final String content;
+
+  const _WalletKeyInfoWidget(
+      {Key? key, required this.title, required this.content, this.titleColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<WalletThemeProvider>(
+      builder: (context, provider, child) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: ROITitleLargeTextStyle(
+                color: titleColor ?? provider.themeMode.text60),
+          ),
+          const SizedBox(height: 4),
+          Container(
+            padding: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+                border:
+                    Border.all(width: 1, color: provider.themeMode.border),
+                borderRadius: const BorderRadius.all(Radius.circular(8))),
+            child: Text(
+              content,
+              style: ROIBodySmallTextStyle(color: provider.themeMode.text),
+            ),
+          )
+        ],
       ),
     );
   }
