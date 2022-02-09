5b8a18b261bf20a13ea7a23ac4a77e488bf39487
VietTH <anhxtanhwp@gmail.com>
2022-02-09 09:52:45 +0700

    update earn

--- lib/features/earn/earn.dart
+++ lib/features/earn/earn.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:wallet/generated/l10n.dart';
+import 'package:wallet/themes/buttons.dart';
+import 'package:wallet/themes/colors.dart';
 import 'package:wallet/themes/theme.dart';
+import 'package:wallet/themes/typography.dart';
 
 class EarnScreen extends StatefulWidget {
   const EarnScreen({Key? key}) : super(key: key);
@@ -18,7 +22,86 @@ class _EarnScreenState extends State<EarnScreen> {
           child: SingleChildScrollView(
             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
             child: Column(
-              children: [],
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: provider.themeMode.bg,
+                    borderRadius: const BorderRadius.all(Radius.circular(16)),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        Strings.current.sharedDelegate,
+                        style: EZCTitleLargeTextStyle(
+                            color: provider.themeMode.text),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        Strings.current.earnDelegateDescription,
+                        style: EZCBodyLargeTextStyle(
+                            color: provider.themeMode.text70),
+                      ),
+                      const SizedBox(height: 16),
+                      Container(
+                        width: double.infinity,
+                        height: 52,
+                        decoration: BoxDecoration(
+                          color: provider.themeMode.primary10,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(4)),
+                          border:
+                              Border.all(color: provider.themeMode.primary80),
+                        ),
+                        child: Stack(
+                          children: [
+                            Container(
+                              width: 4,
+                              height: double.infinity,
+                              decoration: BoxDecoration(
+                                borderRadius: const BorderRadius.only(
+                                    topLeft: Radius.circular(4),
+                                    bottomLeft: Radius.circular(4)),
+                                color: provider.themeMode.primary,
+                              ),
+                            ),
+                            Align(
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 16),
+                                child: Text(
+                                  Strings.current.earnDelegateValidMess,
+                                  style: EZCLabelMediumTextStyle(
+                                      color: provider.themeMode.primary),
+                                ),
+                              ),
+                              alignment: Alignment.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      EZCMediumPrimaryButton(
+                        width: 130,
+                        text: Strings.current.earnDelegateAdd,
+                        enabled: false,
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    EZCMediumNoneButton(
+                      text: Strings.current.earnCrossTransfer,
+                    ),
+                    EZCMediumNoneButton(
+                      text: Strings.current.earnEstimatedRewards,
+                    )
+                  ],
+                ),
+              ],
             ),
           ),
         ),
