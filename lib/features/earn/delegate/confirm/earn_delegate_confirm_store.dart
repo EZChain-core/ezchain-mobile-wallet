c6d76da6aedd4422b9f8284dc7207a9bf7a3835a
vietth <anhxtanhwp@gmail.com>
2022-02-17 09:41:28 +0700

    update earn

--- lib/features/earn/delegate/confirm/earn_delegate_confirm_store.dart
+++ lib/features/earn/delegate/confirm/earn_delegate_confirm_store.dart
@@ -1,6 +1,9 @@
 import 'package:mobx/mobx.dart';
+import 'package:wallet/common/logger.dart';
 import 'package:wallet/di/di.dart';
 import 'package:wallet/features/common/wallet_factory.dart';
+import 'package:wallet/features/earn/delegate/confirm/earn_delegate_confirm.dart';
+import 'package:wallet/roi/wallet/utils/number_utils.dart';
 
 part 'earn_delegate_confirm_store.g.dart';
 
@@ -8,6 +11,30 @@ class EarnDelegateConfirmStore = _EarnDelegateConfirmStore
     with _$EarnDelegateConfirmStore;
 
 abstract class _EarnDelegateConfirmStore with Store {
-  final wallet = getIt<WalletFactory>().activeWallet;
-}
+  final _wallet = getIt<WalletFactory>().activeWallet;
 
+  @observable
+  bool isLoading = false;
+
+  @action
+  delegate(EarnDelegateConfirmArgs args) async {
+    try {
+      final nodeId = args.nodeId;
+      final amount = numberToBNAvaxX(args.amount.toDouble());
+
+      final start = args.startDate.millisecondsSinceEpoch;
+      final end = args.endDate.millisecondsSinceEpoch;
+      isLoading = true;
+      logger.i("nodeId = $nodeId");
+      logger.i("start = $start");
+      logger.i("end = $end");
+      logger.i("amount = $amount");
+      final txId = await _wallet.delegate(nodeId, amount, start, end);
+
+      logger.i("txId = $txId");
+    } catch (e) {
+      logger.e(e);
+    }
+    isLoading = false;
+  }
+}
