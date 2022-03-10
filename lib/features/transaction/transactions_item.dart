6f7040e82c26bb484160f1052b48c89795df987c
KienHT <kienhantrung@gmail.com>
2022-03-11 01:32:22 +0700

    update new tx list UI

--- lib/features/transaction/transactions_item.dart
+++ lib/features/transaction/transactions_item.dart
@@ -7,8 +7,11 @@ import 'package:wallet/common/logger.dart';
 import 'package:wallet/common/router.dart';
 import 'package:wallet/common/router.gr.dart';
 import 'package:wallet/ezc/sdk/apis/pvm/model/get_current_validators.dart';
+import 'package:wallet/ezc/wallet/asset/assets.dart';
 import 'package:wallet/ezc/wallet/explorer/cchain/types.dart';
+import 'package:wallet/ezc/wallet/explorer/ortelius/types.dart';
 import 'package:wallet/ezc/wallet/history/types.dart';
+import 'package:wallet/ezc/wallet/network/helpers/alias_from_network_id.dart';
 import 'package:wallet/ezc/wallet/utils/number_utils.dart';
 import 'package:wallet/features/common/chain_type/ezc_type.dart';
 import 'package:wallet/generated/assets.gen.dart';
@@ -52,79 +55,95 @@ class TransactionsItemWidget extends StatelessWidget {
       builder: (context, provider, child) => TextButton(
         onPressed: onPressed,
         style: TextButton.styleFrom(
-            padding: const EdgeInsets.all(0),
-            splashFactory: NoSplash.splashFactory),
+          padding: const EdgeInsets.all(0),
+          splashFactory: NoSplash.splashFactory,
+        ),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               children: [
-                Column(
-                  children: [
-                    Text(
-                      item.id.useCorrectEllipsis(),
-                      style: EZCTitleLargeTextStyle(
-                          color: provider.themeMode.text),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    const SizedBox(height: 6),
-                    Row(
-                      children: [
-                        Text(
-                          item.time,
-                          style: EZCLabelMediumTextStyle(
-                              color: provider.themeMode.text60),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        item.id.useCorrectEllipsis(),
+                        style: EZCTitleLargeTextStyle(
+                          color: provider.themeMode.text,
                         ),
-                        const SizedBox(width: 10),
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 8, vertical: 2),
-                          decoration: BoxDecoration(
-                            color: provider.themeMode.bg,
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(10)),
-                          ),
-                          child: Text(
-                            item.type,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(height: 6),
+                      Row(
+                        children: [
+                          Text(
+                            item.time,
                             style: EZCLabelMediumTextStyle(
-                                color: provider.themeMode.text60),
+                              color: provider.themeMode.text60,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                          const SizedBox(width: 10),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 8,
+                              vertical: 2,
+                            ),
+                            decoration: BoxDecoration(
+                              color: provider.themeMode.bg,
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                            child: Text(
+                              item.type,
+                              style: EZCLabelMediumTextStyle(
+                                color: provider.themeMode.text60,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  flex: 1,
                 ),
                 const SizedBox(width: 50),
-                Assets.icons.icSearchBlack.svg()
+                Assets.icons.icSearchBlack.svg(),
               ],
             ),
             if (item.from.isNotEmpty) ...[
               const SizedBox(height: 10),
               Text(
                 '${Strings.current.sharedFrom}:',
-                style:
-                    EZCTitleMediumTextStyle(color: provider.themeMode.text60),
+                style: EZCTitleMediumTextStyle(
+                  color: provider.themeMode.text60,
+                ),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
               ),
               const SizedBox(height: 8),
-              Column(
-                children: item.from
-                    .map((e) => TransactionsItemAddressInfoWidget(info: e))
-                    .toList(),
-              ),
+              ...item.from
+                  .map((e) => TransactionsItemAddressInfoWidget(info: e))
+                  .toList()
             ],
             if (item.to.isNotEmpty) ...[
               const SizedBox(height: 10),
               Text(
                 '${Strings.current.sharedTo}:',
-                style:
-                    EZCTitleMediumTextStyle(color: provider.themeMode.text60),
+                style: EZCTitleMediumTextStyle(
+                  color: provider.themeMode.text60,
+                ),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
               ),
               const SizedBox(height: 8),
-              Column(
-                children: item.to
-                    .map((e) => TransactionsItemAddressInfoWidget(info: e))
-                    .toList(),
-              ),
+              ...item.to
+                  .map((e) => TransactionsItemAddressInfoWidget(info: e))
+                  .toList()
             ],
           ],
         ),
@@ -150,17 +169,40 @@ class TransactionsItemAddressInfoWidget extends StatelessWidget {
           borderRadius: const BorderRadius.all(Radius.circular(8)),
         ),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.baseline,
+          mainAxisSize: MainAxisSize.min,
+          textBaseline: TextBaseline.ideographic,
           children: [
-            Text(
-              info.address.useCorrectEllipsis(),
-              style: EZCTitleMediumTextStyle(color: provider.themeMode.text),
+            Expanded(
+              child: Text(
+                info.address.useCorrectEllipsis(),
+                style: EZCTitleMediumTextStyle(
+                  color: provider.themeMode.text,
+                ),
+                textAlign: TextAlign.start,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              flex: 4,
             ),
             if (info.amount != null)
-              Text(
-                info.amount!,
-                style: EZCTitleMediumTextStyle(color: provider.themeMode.text),
+              const Spacer(
+                flex: 1,
               ),
+            if (info.amount != null)
+              Expanded(
+                child: Text(
+                  info.amount!,
+                  style: EZCTitleMediumTextStyle(
+                    color: provider.themeMode.text,
+                  ),
+                  textAlign: TextAlign.end,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                flex: 5,
+              )
           ],
         ),
       ),
@@ -190,6 +232,87 @@ Widget buildTransactionWidget(TransactionsItem transaction) {
       }
     },
   );
+}
+
+Future<List<TransactionsItem>> mapToTransactionsItemsV2(
+  List<OrteliusTx> transactions,
+  EZCType chain,
+) async {
+  final futures = transactions.map((transaction) async {
+    final id = transaction.id;
+    final time = transaction.timestamp?.parseDateTime()?.parseTimeAgo() ?? '';
+    final type = transaction.type.toTypeString();
+
+    final from = <TransactionsItemAddressInfo>[];
+    final inputs = transaction.inputs;
+    if (inputs == null || inputs.isEmpty) {
+      from.add(TransactionsItemAddressInfo(
+        "No input UTXOs found for this transaction on the EZChain Explorer",
+      )); // chuyển vào strings.xml
+    } else {
+      inputs.sort((input1, input2) =>
+          input1.output.outputIndex.compareTo(input2.output.outputIndex));
+      for (final input in inputs) {
+        final output = input.output;
+        final chain = idToChainAlias(output.chainId);
+        final addresses =
+            output.addresses?.map((address) => "$chain-$address") ?? [];
+        String? amount;
+        final amountBN = output.amountBN;
+        if (amountBN != BigInt.zero) {
+          final asset = await getAssetDescription(output.assetId);
+          final denomination = int.tryParse(asset.denomination) ?? 0;
+          amount = bnToLocaleString(amountBN, decimals: denomination) +
+              " " +
+              asset.symbol;
+        }
+        from.add(TransactionsItemAddressInfo(
+          addresses.join("\n"),
+          amount,
+        ));
+      }
+    }
+
+    final to = <TransactionsItemAddressInfo>[];
+    final outputs = transaction.outputs;
+    if (outputs == null || outputs.isEmpty) {
+      from.add(TransactionsItemAddressInfo(
+        "No output UTXOs found for this transaction on the EZChain Explorer",
+      )); // chuyển vào strings.xml
+    } else {
+      outputs.sort((output1, output2) =>
+          output1.outputIndex.compareTo(output2.outputIndex));
+      for (final output in outputs) {
+        final chain = idToChainAlias(output.chainId);
+        final addresses =
+            output.addresses?.map((address) => "$chain-$address") ?? [];
+        String? amount;
+        final amountBN = output.amountBN;
+        if (amountBN != BigInt.zero) {
+          final asset = await getAssetDescription(output.assetId);
+          final denomination = int.tryParse(asset.denomination) ?? 0;
+          amount = bnToLocaleString(amountBN, decimals: denomination) +
+              " " +
+              asset.symbol;
+        }
+        to.add(TransactionsItemAddressInfo(
+          addresses.join("\n"),
+          amount,
+        ));
+      }
+    }
+
+    return TransactionsItem(
+      id,
+      time,
+      type,
+      from,
+      to,
+      EZCType.xChain,
+    );
+  });
+
+  return Future.wait(futures);
 }
 
 List<TransactionsItem> mapToTransactionsItem(
@@ -263,8 +386,8 @@ List<TransactionsItem> mapToTransactionsItem(
             final delegators = validator.delegators;
             if (delegators != null) {
               final delegator = delegators
-                  .firstWhere((delegator) => delegator.txId == item.id);
-              potentialReward = delegator.potentialRewardBN;
+                  .firstWhereOrNull((delegator) => delegator.txId == item.id);
+              potentialReward = delegator?.potentialRewardBN;
               addDelegator = "${item.amountDisplayValue} EZC";
               transType = Strings.current.sharedDelegate;
             }
@@ -285,21 +408,42 @@ List<TransactionsItem> mapToTransactionsItem(
 }
 
 List<TransactionsItem> mapCChainToTransactionsItem(
-    List<CChainExplorerTx> cTransactions) {
+  List<CChainExplorerTx> cTransactions,
+  EZCType type,
+) {
   final List<TransactionsItem> transactions = [];
   try {
     for (var tx in cTransactions) {
       final time =
           tx.timeStamp.parseDateTimeFromTimestamp()?.parseTimeAgo() ?? '';
       final blockNumber = '#${tx.blockNumber}';
-      final value = bnToAvaxC(BigInt.tryParse(tx.value) ?? BigInt.zero);
+      final amountBN = BigInt.tryParse(tx.value) ?? BigInt.zero;
       final gasPrice = BigInt.tryParse(tx.gasPrice) ?? BigInt.zero;
       final gasUsed = BigInt.tryParse(tx.gasUsed) ?? BigInt.zero;
       final fee = '${bnToAvaxC(gasPrice * gasUsed)} EZC';
-      final amount = '$value EZC';
-      final transType = '';
-      // transactions.add(TransactionsCChainItem(tx.hash, time, transType,
-      //     blockNumber, tx.from, tx.to, amount, fee, tx));
+      String? amount;
+      if (amountBN != BigInt.zero) {
+        final value = bnToAvaxC(amountBN);
+        amount = '$value EZC';
+      }
+      const transType = 'Transactions';
+      final from = <TransactionsItemAddressInfo>[];
+      if (tx.from.isNotEmpty) {
+        from.add(TransactionsItemAddressInfo(tx.from));
+      }
+      final to = <TransactionsItemAddressInfo>[];
+      if (tx.to.isNotEmpty) {
+        to.add(TransactionsItemAddressInfo(tx.to, amount));
+      }
+      transactions.add(TransactionsItem(
+        tx.hash,
+        time,
+        transType,
+        from,
+        to,
+        type,
+        tx,
+      ));
     }
   } catch (e) {
     logger.e(e);
