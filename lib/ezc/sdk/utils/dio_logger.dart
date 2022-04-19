f2c1a07422d4ff420683dfa4883705be839ca900
KienHT <kienhantrung@gmail.com>
2022-04-19 17:47:44 +0700

    refine logger

--- lib/ezc/sdk/utils/dio_logger.dart
+++ lib/ezc/sdk/utils/dio_logger.dart
@@ -4,12 +4,12 @@ import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 final _logger = Logger(printer: SimplePrinter());
 
 final prettyDioLogger = PrettyDioLogger(
-  requestHeader: true,
-  requestBody: true,
-  responseBody: true,
-  responseHeader: true,
-  error: true,
-  compact: true,
+  requestHeader: false,
+  requestBody: false,
+  responseBody: false,
+  responseHeader: false,
+  error: false,
+  compact: false,
   maxWidth: 200,
   logPrint: (obj) => _logger.i(obj),
 );
