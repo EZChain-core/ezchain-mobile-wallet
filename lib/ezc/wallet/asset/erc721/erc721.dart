9728aea2a36871a6c3d9ce5445ee28107d9ae4d8
KienHT <kienhantrung@gmail.com>
2022-05-20 09:29:31 +0700

    update erc20 && erc721 contract

--- lib/ezc/wallet/asset/erc721/erc721.dart
+++ lib/ezc/wallet/asset/erc721/erc721.dart
@@ -4,14 +4,14 @@ import 'package:web3dart/web3dart.dart' as _i1;
 import 'dart:typed_data' as _i2;
 
 final _contractAbi = _i1.ContractAbi.fromJson(
-    '[{"inputs":[{"internalType":"string","name":"name_","type":"string"},{"internalType":"string","name":"symbol_","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"baseURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"}]',
+    '[{"type":"constructor","stateMutability":"nonpayable","inputs":[]},{"type":"event","name":"Approval","inputs":[{"type":"address","name":"owner","internalType":"address","indexed":true},{"type":"address","name":"approved","internalType":"address","indexed":true},{"type":"uint256","name":"tokenId","internalType":"uint256","indexed":true}],"anonymous":false},{"type":"event","name":"ApprovalForAll","inputs":[{"type":"address","name":"owner","internalType":"address","indexed":true},{"type":"address","name":"operator","internalType":"address","indexed":true},{"type":"bool","name":"approved","internalType":"bool","indexed":false}],"anonymous":false},{"type":"event","name":"Transfer","inputs":[{"type":"address","name":"from","internalType":"address","indexed":true},{"type":"address","name":"to","internalType":"address","indexed":true},{"type":"uint256","name":"tokenId","internalType":"uint256","indexed":true}],"anonymous":false},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"approve","inputs":[{"type":"address","name":"to","internalType":"address"},{"type":"uint256","name":"tokenId","internalType":"uint256"}]},{"type":"function","stateMutability":"nonpayable","outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"awardItem","inputs":[{"type":"address","name":"player","internalType":"address"},{"type":"string","name":"tokenURI","internalType":"string"}]},{"type":"function","stateMutability":"view","outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"balanceOf","inputs":[{"type":"address","name":"owner","internalType":"address"}]},{"type":"function","stateMutability":"view","outputs":[{"type":"address","name":"","internalType":"address"}],"name":"getApproved","inputs":[{"type":"uint256","name":"tokenId","internalType":"uint256"}]},{"type":"function","stateMutability":"view","outputs":[{"type":"bool","name":"","internalType":"bool"}],"name":"isApprovedForAll","inputs":[{"type":"address","name":"owner","internalType":"address"},{"type":"address","name":"operator","internalType":"address"}]},{"type":"function","stateMutability":"view","outputs":[{"type":"string","name":"","internalType":"string"}],"name":"name","inputs":[]},{"type":"function","stateMutability":"view","outputs":[{"type":"address","name":"","internalType":"address"}],"name":"ownerOf","inputs":[{"type":"uint256","name":"tokenId","internalType":"uint256"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"safeTransferFrom","inputs":[{"type":"address","name":"from","internalType":"address"},{"type":"address","name":"to","internalType":"address"},{"type":"uint256","name":"tokenId","internalType":"uint256"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"safeTransferFrom","inputs":[{"type":"address","name":"from","internalType":"address"},{"type":"address","name":"to","internalType":"address"},{"type":"uint256","name":"tokenId","internalType":"uint256"},{"type":"bytes","name":"_data","internalType":"bytes"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"setApprovalForAll","inputs":[{"type":"address","name":"operator","internalType":"address"},{"type":"bool","name":"approved","internalType":"bool"}]},{"type":"function","stateMutability":"view","outputs":[{"type":"bool","name":"","internalType":"bool"}],"name":"supportsInterface","inputs":[{"type":"bytes4","name":"interfaceId","internalType":"bytes4"}]},{"type":"function","stateMutability":"view","outputs":[{"type":"string","name":"","internalType":"string"}],"name":"symbol","inputs":[]},{"type":"function","stateMutability":"view","outputs":[{"type":"string","name":"","internalType":"string"}],"name":"tokenURI","inputs":[{"type":"uint256","name":"tokenId","internalType":"uint256"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"transferFrom","inputs":[{"type":"address","name":"from","internalType":"address"},{"type":"address","name":"to","internalType":"address"},{"type":"uint256","name":"tokenId","internalType":"uint256"}]}]',
     'ERC721');
 
 class ERC721 extends _i1.GeneratedContract {
   ERC721(
       {required _i1.EthereumAddress address,
-      required _i1.Web3Client client,
-      int? chainId})
+        required _i1.Web3Client client,
+        int? chainId})
       : super(_i1.DeployedContract(_contractAbi, address), client, chainId);
 
   /// The optional [transaction] parameter can be used to override parameters
@@ -19,10 +19,22 @@ class ERC721 extends _i1.GeneratedContract {
   /// set by the contract.
   Future<String> approve(_i1.EthereumAddress to, BigInt tokenId,
       {required _i1.Credentials credentials,
-      _i1.Transaction? transaction}) async {
+        _i1.Transaction? transaction}) async {
     final function = self.abi.functions[1];
     assert(checkSignature(function, '095ea7b3'));
     final params = [to, tokenId];
+    return write(credentials, transaction, function, params);
+  }
+
+  /// The optional [transaction] parameter can be used to override parameters
+  /// like the gas price, nonce and max gas. The `data` and `to` fields will be
+  /// set by the contract.
+  Future<String> awardItem(_i1.EthereumAddress player, String tokenURI,
+      {required _i1.Credentials credentials,
+        _i1.Transaction? transaction}) async {
+    final function = self.abi.functions[2];
+    assert(checkSignature(function, 'cf378343'));
+    final params = [player, tokenURI];
     return write(credentials, transaction, function, params);
   }
 
@@ -31,22 +43,11 @@ class ERC721 extends _i1.GeneratedContract {
   /// latest on-chain block will be used.
   Future<BigInt> balanceOf(_i1.EthereumAddress owner,
       {_i1.BlockNum? atBlock}) async {
-    final function = self.abi.functions[2];
+    final function = self.abi.functions[3];
     assert(checkSignature(function, '70a08231'));
     final params = [owner];
     final response = await read(function, params, atBlock);
     return (response[0] as BigInt);
-  }
-
-  /// The optional [atBlock] parameter can be used to view historical data. When
-  /// set, the function will be evaluated in the specified block. By default, the
-  /// latest on-chain block will be used.
-  Future<String> baseURI({_i1.BlockNum? atBlock}) async {
-    final function = self.abi.functions[3];
-    assert(checkSignature(function, '6c0360eb'));
-    final params = [];
-    final response = await read(function, params, atBlock);
-    return (response[0] as String);
   }
 
   /// The optional [atBlock] parameter can be used to view historical data. When
@@ -103,7 +104,7 @@ class ERC721 extends _i1.GeneratedContract {
   Future<String> safeTransferFrom(
       _i1.EthereumAddress from, _i1.EthereumAddress to, BigInt tokenId,
       {required _i1.Credentials credentials,
-      _i1.Transaction? transaction}) async {
+        _i1.Transaction? transaction}) async {
     final function = self.abi.functions[8];
     assert(checkSignature(function, '42842e0e'));
     final params = [from, to, tokenId];
@@ -116,7 +117,7 @@ class ERC721 extends _i1.GeneratedContract {
   Future<String> safeTransferFrom$2(_i1.EthereumAddress from,
       _i1.EthereumAddress to, BigInt tokenId, _i2.Uint8List _data,
       {required _i1.Credentials credentials,
-      _i1.Transaction? transaction}) async {
+        _i1.Transaction? transaction}) async {
     final function = self.abi.functions[9];
     assert(checkSignature(function, 'b88d4fde'));
     final params = [from, to, tokenId, _data];
@@ -128,7 +129,7 @@ class ERC721 extends _i1.GeneratedContract {
   /// set by the contract.
   Future<String> setApprovalForAll(_i1.EthereumAddress operator, bool approved,
       {required _i1.Credentials credentials,
-      _i1.Transaction? transaction}) async {
+        _i1.Transaction? transaction}) async {
     final function = self.abi.functions[10];
     assert(checkSignature(function, 'a22cb465'));
     final params = [operator, approved];
@@ -161,46 +162,12 @@ class ERC721 extends _i1.GeneratedContract {
   /// The optional [atBlock] parameter can be used to view historical data. When
   /// set, the function will be evaluated in the specified block. By default, the
   /// latest on-chain block will be used.
-  Future<BigInt> tokenByIndex(BigInt index, {_i1.BlockNum? atBlock}) async {
-    final function = self.abi.functions[13];
-    assert(checkSignature(function, '4f6ccce7'));
-    final params = [index];
-    final response = await read(function, params, atBlock);
-    return (response[0] as BigInt);
-  }
-
-  /// The optional [atBlock] parameter can be used to view historical data. When
-  /// set, the function will be evaluated in the specified block. By default, the
-  /// latest on-chain block will be used.
-  Future<BigInt> tokenOfOwnerByIndex(_i1.EthereumAddress owner, BigInt index,
-      {_i1.BlockNum? atBlock}) async {
-    final function = self.abi.functions[14];
-    assert(checkSignature(function, '2f745c59'));
-    final params = [owner, index];
-    final response = await read(function, params, atBlock);
-    return (response[0] as BigInt);
-  }
-
-  /// The optional [atBlock] parameter can be used to view historical data. When
-  /// set, the function will be evaluated in the specified block. By default, the
-  /// latest on-chain block will be used.
   Future<String> tokenURI(BigInt tokenId, {_i1.BlockNum? atBlock}) async {
-    final function = self.abi.functions[15];
+    final function = self.abi.functions[13];
     assert(checkSignature(function, 'c87b56dd'));
     final params = [tokenId];
     final response = await read(function, params, atBlock);
     return (response[0] as String);
-  }
-
-  /// The optional [atBlock] parameter can be used to view historical data. When
-  /// set, the function will be evaluated in the specified block. By default, the
-  /// latest on-chain block will be used.
-  Future<BigInt> totalSupply({_i1.BlockNum? atBlock}) async {
-    final function = self.abi.functions[16];
-    assert(checkSignature(function, '18160ddd'));
-    final params = [];
-    final response = await read(function, params, atBlock);
-    return (response[0] as BigInt);
   }
 
   /// The optional [transaction] parameter can be used to override parameters
@@ -209,8 +176,8 @@ class ERC721 extends _i1.GeneratedContract {
   Future<String> transferFrom(
       _i1.EthereumAddress from, _i1.EthereumAddress to, BigInt tokenId,
       {required _i1.Credentials credentials,
-      _i1.Transaction? transaction}) async {
-    final function = self.abi.functions[17];
+        _i1.Transaction? transaction}) async {
+    final function = self.abi.functions[14];
     assert(checkSignature(function, '23b872dd'));
     final params = [from, to, tokenId];
     return write(credentials, transaction, function, params);
